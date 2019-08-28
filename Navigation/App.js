ea39fd78434c365a60c7df41bbe9d8cc72e6e4c5
narawitEarth <53513653+narawitEarth@users.noreply.github.com>
2019-08-28 15:59:58 +0700

    Add files via upload

---
+++ Navigation/App.js
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+import StackNavigator from './Stack/stacknavigator'
+
+
+
+// You can import from local files
+import AssetExample from './components/AssetExample';
+
+// or any pure javascript modules available in npm
+import { Card } from 'react-native-paper';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <StackNavigator/>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+});
