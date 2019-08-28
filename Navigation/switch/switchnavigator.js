e6ebfb08ac085daefe8bf5f0d27cfb6aa8e937e6
narawitEarth <53513653+narawitEarth@users.noreply.github.com>
2019-08-28 16:14:30 +0700

    Add files via upload

---
+++ Navigation/switch/switchnavigator.js
@@ -0,0 +1,17 @@
+import { createSwitchNavigator, createAppContainer} from 'react-navigation';
+import Page1 from './page1'
+import Page2 from './page2'
+// import Page3 from './page3'
+
+const stack = createSwitchNavigator ({ 
+  Page1Screen:{screen:Page1,navigationOptions: {
+  title:'Page1'}},
+  Page2Screen:{screen:Page2,navigationOptions: {
+  title:'Page2'}},
+  // Page3Screen:{screen:Page3,navigationOptions: {
+  // title:'Page3'}},
+});
+
+
+
+export default createAppContainer(stack);
